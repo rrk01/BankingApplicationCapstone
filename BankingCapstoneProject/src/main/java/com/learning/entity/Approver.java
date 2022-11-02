329493c62943342e12f4daf5bffa481503ea73ab
VADIM <79195442+xrustlex@users.noreply.github.com>
2022-11-02 15:10:35 -0400

    Update Approver.java
    updated Approver.class

--- BankingCapstoneProject/src/main/java/com/learning/entity/Approver.java
+++ BankingCapstoneProject/src/main/java/com/learning/entity/Approver.java
@@ -1,5 +1,45 @@
 package com.learning.entity;
 
-public class Approver {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="approvers")
+public class Approver {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(name="name")
+	private String name;
+	
+	public Approver() {
+		super();
+	}
+
+	public Approver(long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
