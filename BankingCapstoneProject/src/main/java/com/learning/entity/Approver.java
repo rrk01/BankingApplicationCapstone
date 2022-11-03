dacd363f9d1bb47ab3d0cd86f6b36c7c87ab78e2
dick <dick@mbp-vadim.lan>
2022-11-03 02:24:50 -0400

    fixed Approver

--- BankingCapstoneProject/src/main/java/com/learning/entity/Approver.java
+++ BankingCapstoneProject/src/main/java/com/learning/entity/Approver.java
@@ -8,6 +8,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name="approvers")
